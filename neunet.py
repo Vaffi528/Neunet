0633f527290b4abae5ddea27a2570e80ee115665
Vaffi528 <trukhalex8@gmail.com>
2025-07-06 04:49:32 +0300

    feat: added new version of optimized back prop method

--- neunet.py
+++ neunet.py
@@ -17,8 +17,11 @@ class Network():
         np.random.seed(1)
         self.weights_all = [2*np.random.random((neurons[i],neurons[i+1]))-1 for i in range(len(neurons)-1)]
         self.bias_indexes = bias
-        self.deltas = [np.zeros((neurons[i],neurons[i+1])) for i in range(len(neurons)-1)]
-        self.deltas_bias = [np.zeros((1,neurons[index+1])) for index in bias]
+        '''self.deltas = [np.zeros((neurons[i],neurons[i+1])) for i in range(len(neurons)-1)]
+        self.deltas_bias = [np.zeros((1,neurons[index+1])) for index in bias]'''
+        self.neurons_amount = neurons
+        self.neurons = [[np.zeros(index) for index in neurons[:-1]] for i in range(1000)]
+        self.deltas = [[np.zeros(index) for index in list(reversed(neurons[1:]))] for i in range(1000)]
         if bias != []:
             try:
                 self.bias_all = [2*np.random.random((1,neurons[index+1]))-1 for index in bias]
@@ -44,6 +47,25 @@ class Network():
             #randomising data to preventing nn form wrong regularities
             np.random.shuffle(train)
             for it, element in enumerate(train):
+                if it % 1000 == 0 and it != 0:
+                    iterations = 1000
+                    deltas_neurons = [[self.deltas[i][j] * self.neurons[i][-(j+1)].T  for j in range(len(self.neurons[0]))] for i in range(iterations)]
+                    deltas_neurons_sum = [np.squeeze(np.sum(np.array([[deltas_neurons[j][i]] for j in range(len(deltas_neurons))])), axis=0) for i in range(len(deltas_neurons[0]))]
+                    deltas_sum = [np.sum(np.array([[np.squeeze(self.deltas[j][i])] for j in range(iterations)]), axis=0) for i in range(len(self.deltas[0]))]
+                    #recent index of bias neuron's weights sequence number
+                    bias_index = -1
+
+                    #all the layer's weights adjustment
+                    for i in range(len(self.weights_all)):
+                        self.weights_all[-(i+1)] -= self.l * deltas_neurons_sum[i]
+                        #biases weights adjustment
+                        if (len(self.weights_all)-1)-i in self.bias_indexes:
+                            self.bias_all[bias_index] -= self.l * deltas_sum[i]
+                            bias_index -= 1
+
+                    self.neurons = [[np.zeros(index) for index in self.neurons_amount[:-1]] for i in range(1000 if len(train) - it >= 1000 else len(train)%1000)]
+                    self.deltas = [[np.zeros(index) for index in list(reversed(self.neurons_amount[1:]))] for i in range(1000 if len(train) - it >= 1000 else len(train)%1000)]
+
                 #list of the values of neurons from all the layers
                 neurons = [np.array([element[0:lens]])]
                 #recent index of bias neuron's weights sequence number
@@ -63,7 +85,7 @@ class Network():
                 #error calculation
                 err = neurons[-1] - np.array([element[lens:]])
                 #list of the values of deltas form all the layers
-                deltas = [err * self.derivative(neurons[-1])]
+                '''deltas = [err * self.derivative(neurons[-1])]
                 
                 #recent index of bias neuron's delta's sequence number
                 bias_index = -1
@@ -89,7 +111,31 @@ class Network():
                             bias_index -= 1
 
                     for i in range(len(neurons)-1): self.deltas[i] *= 0 
-                    for i in range(len(self.deltas_bias)): self.deltas_bias[i] *= 0 
+                    for i in range(len(self.deltas_bias)): self.deltas_bias[i] *= 0 '''
+
+                self.neurons[it%1000] = neurons[:-1]
+                self.deltas[it%1000][0] = err * self.derivative(neurons[-1])
+                for i in range(1, len(self.weights_all)): 
+                    self.deltas[it%1000][i] = (self.deltas[it%1000][i-1] @ self.weights_all[-(i)].T) * self.derivative(neurons[-(i+1)])
+                
+                if it == len(train)-1:
+                    iterations = it%1000
+                    deltas_neurons = [[self.deltas[i][j] * self.neurons[i][-(j+1)].T  for j in range(len(self.neurons[0]))] for i in range(iterations)]
+                    deltas_neurons_sum = [np.squeeze(np.sum(np.array([[deltas_neurons[j][i]] for j in range(len(deltas_neurons))])), axis=0) for i in range(len(deltas_neurons[0]))]
+                    deltas_sum = [np.sum(np.array([[np.squeeze(self.deltas[j][i])] for j in range(iterations)]), axis=0) for i in range(len(self.deltas[0]))]
+                    #recent index of bias neuron's weights sequence number
+                    bias_index = -1
+
+                    #all the layer's weights adjustment
+                    for i in range(len(self.weights_all)):
+                        self.weights_all[-(i+1)] -= self.l * deltas_neurons_sum[i]
+                        #biases weights adjustment
+                        if (len(self.weights_all)-1)-i in self.bias_indexes:
+                            self.bias_all[bias_index] -= self.l * deltas_sum[i]
+                            bias_index -= 1
+
+                    self.neurons = [[np.zeros(index) for index in self.neurons_amount[:-1]] for i in range(1000 if len(train) >= 1000 else len(train)%1000)]
+                    self.deltas = [[np.zeros(index) for index in list(reversed(self.neurons_amount[1:]))] for i in range(1000 if len(train) >= 1000 else len(train)%1000)]
 
             if e - int(epo/4) == 0 or e - int(epo/2) == 0 or e - int(epo/1.333333333) == 0:
                 print(f'done: {round(e/epo, 2)*100}%')
